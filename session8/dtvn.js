adc2eed3bbba67acbdbcfe70c066a8b4f1b8106c
Chad Luong <apple.luong1905@gmail.com>
2018-12-14 19:47:27 +0700

    update js

---
+++ session8/dtvn.js
@@ -0,0 +1,13 @@
+function showPlayer(player) {
+	switch (player) {
+		case 'cp':
+				document.getElementById('name').innerHTML = "Cong Phuong";
+				break;
+		case 'vd':
+				document.getElementById('name').innerHTML = "Van Duc";
+				break;
+		default:
+				break;
+
+	}
+}
